7323022af923abf2128fa47cb0450f1ae759d249
dudtivan <ivandustinbriones@gmail.com>
2026-05-25 17:07:42 +0800

    feedback

--- api/messages.js
+++ api/messages.js
@@ -7,8 +7,8 @@ export default async function handler(req) {
   const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
   const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-  let fetchUrl = `${SUPABASE_URL}/rest/v1/feedback_messages?order=created_at.asc`
-  if (user_id) fetchUrl += `&user_id=eq.${user_id}`
+  let fetchUrl = `${SUPABASE_URL}/rest/v1/feedback_with_email?order=created_at.asc`
+if (user_id) fetchUrl += `&user_id=eq.${user_id}`
 
   const res = await fetch(fetchUrl, {
     headers: {
